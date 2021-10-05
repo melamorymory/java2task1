d140c5030c52b188d77f7355bcd54edd52c5016b
Olga Lebedeva <birdswann@gmail.com>
2021-10-05 17:19:10 +0300

    Task 1 done

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10_000;
+        int priceOfMile = 20;
+        int bonusMiles = price / priceOfMile;
+        System.out.println(bonusMiles);
+    }
+}
